b174586cefa666bf93c75937a016d2800703490f
Khmelevskoi Oleh <oleh@KhmelevskoisAir.fritz.box>
2024-09-16 20:02:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-sm font-bold underline text-black">Hello world!</h1>
+  </header>
+  <main></main>
+  <footer></footer>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="p-4 text-3xl bg-pink-100">HOME/</div>
+  </main>
+</template>
